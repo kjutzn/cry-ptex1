f69093e4dda3c793b4d1c91accf1b70c9e03e886
Kjutzn <130908680+kjutzn@users.noreply.github.com>
2023-10-22 17:20:34 +0200

    Replaced not working dfu helper with palera1n -D

--- macos.sh
+++ macos.sh
@@ -1,148 +1,18 @@
 #!/bin/bash
 
-## all variables: script_path, installsshrd, act_have, act_path
-
-_info() {
-    if [ "$1" = 'recovery' ]; then
-        echo $("$dir"/irecovery -q | grep "$2" | sed "s/$2: //")
-    elif [ "$1" = 'normal' ]; then
-        echo $("$dir"/ideviceinfo | grep "$2: " | sed "s/$2: //")
-    fi
-}
-
-step() {
-    rm -f .entered_dfu
-    for i in $(seq "$1" -1 0); do
-        if [[ -e .entered_dfu ]]; then
-            rm -f .entered_dfu
-            break
-        fi
-        if [[ $(get_device_mode) == "dfu" || ($1 == "10" && $(get_device_mode) != "none") ]]; then
-            touch .entered_dfu
-        fi &
-        printf '\r\e[K\e[1;36m%s (%d)' "$2" "$i"
-        sleep 1
-    done
-    printf '\e[0m\n'
-}
-
-get_device_mode() {
-    if [ "$os" = "Darwin" ]; then
-        sp="$(system_profiler SPUSBDataType 2> /dev/null)"
-        apples="$(printf '%s' "$sp" | grep -B1 'Vendor ID: 0x05ac' | grep 'Product ID:' | cut -dx -f2 | cut -d' ' -f1 | tail -r)"
-    elif [ "$os" = "Linux" ]; then
-        apples="$(lsusb | cut -d' ' -f6 | grep '05ac:' | cut -d: -f2)"
-    fi
-    local device_count=0
-    local usbserials=""
-    for apple in $apples; do
-        case "$apple" in
-            12a8|12aa|12ab)
-            device_mode=normal
-            device_count=$((device_count+1))
-            ;;
-            1281)
-            device_mode=recovery
-            device_count=$((device_count+1))
-            ;;
-            1227)
-            device_mode=dfu
-            device_count=$((device_count+1))
-            ;;
-            1222)
-            device_mode=diag
-            device_count=$((device_count+1))
-            ;;
-            1338)
-            device_mode=checkra1n_stage2
-            device_count=$((device_count+1))
-            ;;
-            4141)
-            device_mode=pongo
-            device_count=$((device_count+1))
-            ;;
-        esac
-    done
-    if [ "$device_count" = "0" ]; then
-        device_mode=none
-    elif [ "$device_count" -ge "2" ]; then
-        echo "[-] Please attach only one device" > /dev/tty
-        kill -30 0
-        exit 1;
-    fi
-    if [ "$os" = "Linux" ]; then
-        usbserials=$(cat /sys/bus/usb/devices/*/serial)
-    elif [ "$os" = "Darwin" ]; then
-        usbserials=$(printf '%s' "$sp" | grep 'Serial Number' | cut -d: -f2- | sed 's/ //')
-    fi
-    if grep -qE '(ramdisk tool|SSHRD_Script) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{1,2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}' <<< "$usbserials"; then
-        device_mode=ramdisk
-    fi
-    echo "$device_mode"
-}
-
-_dfuhelper() {
-    if [ "$(get_device_mode)" = "dfu" ]; then
-        echo "[*] Device is already in DFU"
-        return
-    fi
-
-    local step_one;
-    deviceid=$( [ -z "$deviceid" ] && _info normal ProductType || echo $deviceid )
-
-    if [[ "$1" = 0x801* && "$deviceid" != *"iPad"* ]]; then
-        step_one="Hold volume down + side button"
-    else
-        step_one="Hold home + power button"
-    fi
-
-    if $dfuhelper_first_try; then
-        echo "[*] Press any key when ready for DFU mode"
-        read -n 1 -s
-        dfuhelper_first_try=false
-    fi
-
-    step 3 "Get ready"
-    step 4 "$step_one" &
-    sleep 2
-    "$dir"/irecovery -c "reset" &
-    wait
-
-    if [[ "$1" = 0x801* && "$deviceid" != *"iPad"* ]]; then
-        step 10 'Release side button, but keep holding volume down'
-    else
-        step 10 'Release power button, but keep holding home button'
-    fi
-
-    sleep 1
-    
-    if [ "$(get_device_mode)" = "dfu" ]; then
-        echo "[*] Device entered DFU!"
-        dfuhelper_first_try=true
-    else
-        echo "[-] Device did not enter DFU mode"
-        return -1
-    fi
-}
-
 script_path="$(cd "$(dirname "$0")" && pwd)"
 
-echo "== Cry Ptex1 =="
+echo "== Cry ptex1 =="
 echo "Made by Kjutzn"
-echo "Thanks to Orangera1n, VeryGenericName (ssh)"
+echo "Thanks to Orangera1n, VeryGenericName (ssh), Palera1n"
 echo "This tool DOES NOT work on iPhone X, due to sep breaking restores."
-
 echo
+echo "Please open the guide in another tab and follow it!"
+echo "If you need any help or run into any problems, open an issue on GitHub or contact me on Discord."
 
-echo "Please open the guide in another tab and follow it closely!"
-echo "If you need any help, open a new issue on GitHub or contact me on Discord."
-echo
+echo "What version is your iPhone on?"
+read ios1
 
-##echo "What version is your iPhone on?"
-##read ios1
-##echo
-
-##echo "Hello, $ios1! Welcome to the script." ## this is just for testing input!!!
 ##echo $script_path
 ##echo
 
@@ -159,9 +29,8 @@ else
     read installsshrd
 
     if [ "$installsshrd" = "y" ]; then
-        echo "Cloning verygenericname/SSHRD_Script"
+        echo "Cloning SSHRD_Script... Please wait!"
         git clone https://github.com/verygenericname/SSHRD_Script --recursive
-        
         cd "$script_path/SSHRD_Script" && git pull
 
     else
@@ -172,7 +41,14 @@ fi
 
 ## SSHRD exists and it is up to date! Script can continue.
 
-echo
+if [ ! -f "$script_path/sshpass" ]; then
+    cp "$script_path/SSHRD_Script/Darwin/sshpass" "$script_path/"
+else
+    echo 
+fi
+
+## sshpass is in $script_path
+
 echo "[?] Do you have activation files? (y/n)"
 read act_have
 
@@ -185,7 +61,11 @@ if [ "$act_have" = "y" ]; then
     else
         echo
         echo "[*] Plug you device and trust it"
-         _dfuhelper    ##disabled for now!!!!
+
+        echo
+        echo "[!] New terminal window should pop up, follow instructions on entering DFU Mode"
+        osascript -e "tell application \"Terminal\" to do script \"palera1n -D\""
+
         cd $script_path/SSHRD_Script && chmod +x sshrd.sh && ./sshrd.sh $ios1
         cd $script_path/SSHRD_Script && ./sshrd.sh boot
 
@@ -193,6 +73,21 @@ if [ "$act_have" = "y" ]; then
 
         chmod +x "$script_path"/SSHRD_Script/Darwin/iproxy
         chmod +x "$script_path"/SSHRD_Script/Darwin/sshpass
+        chmod +x "$script_path"/sshpass
+
+        sleep 3
+
+        if [ "$resethosts" = "y" ]; then
+            echo "Please enter your username(of this mac): "
+            read usernamemac
+
+            rm -rf /Users/$usernamemac/.ssh/known_hosts
+        
+        else
+            echo "Trying without reseting known_hosts!"
+    fi
+
+        sleep 5
 
         echo "[*] You might have to press allow for opening new terminal window"
         osascript -e "tell application \"Terminal\" to do script \"cd $script_path/SSHRD_Script && ./sshrd.sh ssh\""
@@ -200,14 +95,27 @@ if [ "$act_have" = "y" ]; then
         echo "[!] Do not close it"
         echo
 
+
+
+        sleep 10
+
         echo "[*] Connecting to your device. Downloading Fairplay folder... "
-        sftp -oPort=2222 root@localhost:/mnt2/mobile/Library/Fairplay $script_path/Activation
+        sleep 1
+        sshpass -p 'alpine' sftp -oPort=2222 root@localhost:/mnt2/mobile/Library/Fairplay "$script_path/Activation"
         ## add check
+
+        sleep 5
 
         echo "[*] Downloading commcenter.device_specific_nobackup.plist "
-        sftp -oPort=2222 root@localhost:/mnt2/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist $script_path/Activation
+        sleep 1
+        sshpass -p 'alpine' sftp -oPort=2222 root@localhost:/mnt2/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist "$script_path/Activation"
         ## add check
+        sleep 5
 
-        ssh -p 2222 root@localhost 'cd /mnt2/containers/Data/System; find /mnt2/containers/Data/System -name internal'
+        echo "[*] Check if Fairplay folder and com.apple.commcenter.device_specific_nobackup.plist exist in Activation folder!"
+        sleep 5
 
-    fi
+        echo "[*] Open FileZilla and connect to your iDevice with video guide(It is in GitHub repo."
+        sleep 
+
+fi
