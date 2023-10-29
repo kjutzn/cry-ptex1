f8114fcb1a73d80e7f7b2b741594c3c4500077d2
Kjutzn <130908680+kjutzn@users.noreply.github.com>
2023-10-29 21:16:16 +0100

    Update grabbingFiles.sh

--- grabbingFiles.sh
+++ grabbingFiles.sh
@@ -4,16 +4,15 @@ script_path="$(cd "$(dirname "$0")" && pwd)"
 
 echo "== Cry ptex1 =="
 echo "Made by Kjutzn"
-echo "Thanks to Orangera1n, VeryGenericName (ssh), Palera1n, OpenAI"
+echo "Thanks to Orangera1n, VeryGenericName (ssh), Palera1n"
 echo "This tool DOES NOT work on iPhone X, due to sep breaking restores."
-echo "If you need any help or run into any problems, open an issue on GitHub or contact me on Discord."
 echo
+echo "Please open the guide in another tab and follow it!"
+echo "If you need any help or run into any problems, open an issue on GitHub or contact me on Discord."
 
 echo "[?] What version is your iDevice on?"
 read ios1
-
-##echo $script_path
-##echo
+echo
 
 if [ -d "$script_path/SSHRD_Script" ]; then
     echo "[*] SSHRD_Script exists in the script's directory."
@@ -38,15 +37,18 @@ else
     fi
 fi
 
-## SSHRD exists and it is up to date! Script can continue.
 
 if [ ! -f "$script_path/sshpass" ]; then
     cp "$script_path/SSHRD_Script/Darwin/sshpass" "$script_path/"
 else
-    echo 
+    echo "[*] Copying sshpass to script path (Will be needed for later)"
 fi
 
-## sshpass is in $script_path
+if [ ! -f "$script_path/iproxy" ]; then
+    cp "$script_path/SSHRD_Script/Darwin/iproxy" "$script_path/"
+else
+    echo "[*] Copying iproxy to script path (Will be needed for later)"
+fi
 
 echo "[?] Do you have activation files? (y/n)"
 read act_have
@@ -64,7 +66,7 @@ else
         echo "[!] New terminal window should pop up, follow instructions on entering DFU Mode"
         osascript -e "tell application \"Terminal\" to do script \"palera1n -D\""
 
-        sleep 2
+        sleep2
         cd $script_path/SSHRD_Script && chmod +x sshrd.sh && ./sshrd.sh $ios1
         echo "[*] Booting ramdisk"
         cd $script_path/SSHRD_Script && ./sshrd.sh boot
@@ -73,10 +75,13 @@ else
 
         chmod +x "$script_path"/SSHRD_Script/Darwin/iproxy
         chmod +x "$script_path"/SSHRD_Script/Darwin/sshpass
+        chmod +x "$script_path"/SSHRD_Script/Darwin/iproxy
         chmod +x "$script_path"/sshpass
         chmod +x "$script_path"/activate.sh
+        chmod +x "$script_path"/iproxy
 
-        sleep 3
+
+        sleep3
 
         echo "[*] This part of script resets known_hosts to avoid any ssh issues. "
         echo "[?] Is that alright? (y/n)"
@@ -92,7 +97,7 @@ else
             echo "Trying without reseting known_hosts!"
         fi
 
-        sleep 2
+        sleep2
 
         echo "[*] You might have to press allow for opening new terminal window"
         osascript -e "tell application \"Terminal\" to do script \"cd $script_path/SSHRD_Script && ./sshrd.sh ssh\""
@@ -107,14 +112,14 @@ else
         echo "[*] Connecting to your device. Downloading Fairplay folder... "
         sleep 1
         ./sshpass -p 'alpine' sftp -oPort=2222 -r root@localhost:/mnt2/mobile/Library/FairPlay "$script_path/activation"
-        ## add check
+         add check
 
         sleep 5
 
         echo "[*] Downloading commcenter.device_specific_nobackup.plist "
         sleep 1
         ./sshpass -p 'alpine' sftp -oPort=2222 root@localhost:/mnt2/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist "$script_path/activation"
-        ## add check
+         add check
         sleep 5
 
         echo "[*] Downloading internal"
@@ -123,7 +128,7 @@ else
         ACT3=$ACT1/data_ark.plist
         sleep 1
         ./sshpass -p 'alpine' sftp -oPort=2222 root@localhost:$ACT3 "$script_path/activation"
-        ## add check
+         add check
         sleep 1
 
         echo "[*] Downloading activation_record.plist"
@@ -165,188 +170,11 @@ echo
 echo "[?] Continue? (y/n)"
 read preparedact
 
-sudo chmod -R 755 $script_path/activation
-sleep 3
-
-echo "[*] Creating FakeFS follow Palera1n Instructions in new terminal window. "
-osascript -e "tell application \"Terminal\" to do script \"palera1n -c -f\""
-sleep 3
-
-echo "[!] Enter y when FakeFS finishes creating. "
-read fakefsdone
-sleep 1
-    
-if [ "$fakefsdone" = "y" ]; then
-    echo "Please enter what version did you downgrade to: "
-    read _iosdowngraded
-    sleep 3
-
-    echo "[*] Creating ramdisk for $_iosdowngraded"
-
-    cd $script_path/SSHRD_Script && chmod +x sshrd.sh && ./sshrd.sh $_iosdowngraded
-    sleep 3
-
-    echo "[*] Booting ramdisk"
-    cd $script_path/SSHRD_Script && chmod +x sshrd.sh && ./sshrd.sh boot
-    sleep 3
-
-    osascript -e "tell application \"Terminal\" to do script \"cd $script_path/SSHRD_Script && ./sshrd.sh ssh\""
-    echo "[!] Do not close sshrd ssh terminal"
-
-    echo "[*] Mounting filesystems"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount_filesystems
-    sleep 3
-
-    echo "[?] Does your device have Baseband? (y/n)"
-    echo "[*] Enter y if it isn't iPad"
-    sleep 1
-    read basebandyn
-
-    if [ "$basebandyn" = "y" ]; then
-        echo "[*] Mounting apfs" 
-        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount_apfs /dev/disk0s1s8 /mnt8
-    else
-        echo "[*] Mounting apfs" 
-        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount_apfs /dev/disk0s1s7 /mnt8
-    fi
-
-    echo "[*] ldid mobileactivationd"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 ldid -e /mnt8/usr/libexec/mobileactivationd > /mnt8/ents.xml
-    sleep 1
-
-    echo "[*] Renaming mobileactivationd to mobileactivationd_backup"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mv /mnt8/usr/libexec/mobileactivationd /mnt8/usr/libexec/mobileactivationd_backup
-    sleep 1
-
-    echo "[*] Downloading required files."
-    curl -o "$script_path/mobileactivationd" "https://cdn.discordapp.com/attachments/1020892312756293695/1102082543253205012/mobileactivationd"
-    sleep 3
-
-    echo "[*] Uploading required files to iDevice "
-    ./sshpass -p 'alpine' scp -P 2222 "$script_path/mobileactivationd" root@localhost:/mnt8/usr/libexec/
-
-    echo "[*] Reparing permisions"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 chmod 755 /mnt8/usr/libexec/mobileactivationd
-    sleep 2
-
-    echo "[*] Resigning files"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 cd /mnt8
-    sleep 2
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 ldid -Sents.xml /mnt8/usr/libexec/mobileactivationd
-    sleep 2
-
-    echo "[*] Device should reboot, also do not set passcode or sign in into icloud yet"
-    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 reboot
-    sleep 6
-    echo "[*] Now you need to follow guide in other terminal window with instructions on jailbreaking with palera1n"
-    
-    osascript -e "tell application \"Terminal\" to do script \"palera1n -f\""
-    echo "[*] Opened new Terminal window"
-    echo "[*] Also if you are on Apple Silicon mac you need to replug ligtning cable when you see apple logo on device"
-    sleep 3
-
-    echo "[!] Enter y when device finishes jailbreaking "
-    read finishedjb
-    if [ "$finishedjb" = "y" ]; then
-
-    echo "[*] Then Complete setup as normal, and use the palera1n loader to install Sileo"
-    echo "[!] You MUST set password for sudo commands in palera1n to: alpine"
-    echo "[*] After Bootstrap finishes downloading, open Sileo and install openssh."
-    sleep 3
-        if [ "$installingbootstrap" = "y" ]; then
-                echo "[*] Booting ramdisk"
-                cd $script_path/SSHRD_Script && chmod +x sshrd.sh && ./sshrd.sh boot
-                sleep 3
-
-                echo "[*] SSH window should pop up"
-                osascript -e "tell application \"Terminal\" to do script \"cd $script_path/SSHRD_Script && ./sshrd.sh ssh\""
-                echo "[!] Do not close it"
-                if [ "$basebandyn" = "y" ]; then
-                    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount_apfs /dev/disk0s1s8 /mnt8
-                else
-                    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount_apfs /dev/disk0s1s7 /mnt8
-                fi
-
-                echo 
-                ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mv /mnt8/usr/libexec/mobileactivationd_backup /mnt8/usr/libexec/mobileactivationd
-                sleep 2
-
-                echo "[*] Rebooting, after jailbreak finishes jailbreak with palera1n"
-                ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 reboot
-                echo "[*] Also you can close all opened terminal windows, exept this one"
-                sleep 7
-
-                echo "[*] Opened new palera1n window"
-                osascript -e "tell application \"Terminal\" to do script \"palera1n -f\""
-                echo "[*] Same as last time, if you are on Apple Silicon mac you need to replug ligtning cable when you see apple logo on device"
-                sleep 3
-
-                echo
-                echo "[*] Enter y when device finishes jailbreaking"
-
-                if [ "$finishedjb1" = "y" ]; then
-                    echo "[*] Reseting known_hosts"
-                    rm -rf /Users/$usernamemac/.ssh/known_hosts
-                    sleep 1
-
-                    osascript -e "tell application \"Terminal\" to do script \"cd $script_path/SSHRD_Script && ./sshrd.sh ssh\""
-                    echo "[!] Do not close sshrd ssh terminal"
-
-                    echo
-                    
-                    echo "[*] Do you want script to try to automatically remove internal folders, or you want to do it manually?"
-                    read manualrmrf
-                    if [ "$manualrmrf" = "y" ]; then
-                        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 cd /var/containers/Data/System
-                        sleep 1
-
-                        echo "[*] Internal folders are located in these directories: "
-                        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 find /private/var/containers/Data/System -name internal
-                        sleep 1
-
-                        echo
-                        echo "[*] Open ssh window and enter this command rm -rf and both container numbers(one by one) without EXCLUDING internal"
-                        
-                        echo "[*] Enter y when done"
-                        read donermrf1
-
-                        if [ "$donermrf1" = "y" ]; then
-
-                        else
-                        echo "[!] Exit code: 106"
-                        exit 106
-                        fi
-                    else
-                        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 cd /var/containers/Data/System
-                        ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 find /private/var/containers/Data/System -type d -name internal -prune -o -exec rm -rf {} \; 
-
-                    fi
-
-                    sleep 5
-
-                    echo "[*] Device should reboot now"
-                    ./sshpass -p alpine ssh -o StrictHostKeyChecking=no root@localhost -p 2222 reboot
-                    sleep 6 
-
-                    echo "[*] Wait for phone to reboot (if it doesn't force reboot) and try to activate"
-                    echo "[*] This part of script ends here. When done open next part ./activate.sh"
-                    sleep 3
-                    exit 200
-                else
-                    echo "[!] Exit code: 105"
-                    exit 105
-                fi
-        else
-            echo "[!] Exit code: 104"
-            exit 104
-        fi
-
-    else
-        echo "[!] Exit code: 103"
-        exit 103
-    fi
-
+if [ "$preparedact" = "y" ]; then
+    echo "[*] Starting ./futurerestored.sh, also copy activation folder to any location just in case"
+    echo "[*] Script is continuing in 10 seconds"
+    sleep 10
+    cd $script_path && ./futurerestored.sh
 else
-    echo "[!] Exit code: 102"
-    exit 102
+    exit 160
 fi
